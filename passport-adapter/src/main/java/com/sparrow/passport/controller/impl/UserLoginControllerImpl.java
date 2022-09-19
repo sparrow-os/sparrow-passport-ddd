88745bc6a4e52df0e1a68992577b8d7d652e9d4f
harry <zh_harry@163.com>
2022-09-20 00:55:52 +0800

    init

--- passport-adapter/src/main/java/com/sparrow/passport/controller/impl/UserLoginControllerImpl.java
+++ passport-adapter/src/main/java/com/sparrow/passport/controller/impl/UserLoginControllerImpl.java
@@ -14,6 +14,7 @@ import com.sparrow.passport.api.UserLoginService;
 import com.sparrow.passport.controller.UserLoginController;
 import com.sparrow.passport.controller.assemble.LoginControllerAssemble;
 import com.sparrow.passport.controller.protocol.query.LoginQuery;
+import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.inject.Named;
 
