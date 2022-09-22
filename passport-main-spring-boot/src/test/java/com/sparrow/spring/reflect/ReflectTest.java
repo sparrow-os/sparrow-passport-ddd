a6b3b44dc87c535800ccf1a0233ea77ccda0ddaf
harry <zh_harry@163.com>
2022-09-22 21:04:29 +0800

    add mybatis module

--- passport-main-spring-boot/src/test/java/com/sparrow/spring/reflect/ReflectTest.java
+++ passport-main-spring-boot/src/test/java/com/sparrow/spring/reflect/ReflectTest.java
@@ -4,12 +4,22 @@ import com.sparrow.passport.po.User;
 
 public class ReflectTest {
     public static void main(String[] args) {
-        User user=new User();
-        long t=System.currentTimeMillis();
-        for(int i=0;i<10000000;i++) {
-            user.setUserName("zhangsan");
-            user.getUserName();
+        User user = new User();
+        long t = System.currentTimeMillis();
+        user.setUserName("zhangsan");
+        for (int i = 0; i < 10000000; i++) {
+            get(user, "userName");
         }
-        System.out.println(System.currentTimeMillis()-t);
+        System.out.println(System.currentTimeMillis() - t);
+    }
+
+    private static Object get(User user, String field) {
+        if ("userName".equalsIgnoreCase(field)) {
+            return user.getUserName();
+        }
+        if ("userId".equalsIgnoreCase(field)) {
+            return user.getUserId();
+        }
+        return null;
     }
 }
