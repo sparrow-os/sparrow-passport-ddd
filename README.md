0b6c729662f6e620e5258ac535a1c71af3953171
harry <zh_harry@163.com>
2022-10-18 02:25:35 +0800

    add wechat

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sparrow-passport-ddd
 sparrow-passport-ddd
+
+
+志哥微信
+---
+![wechar](img.png)
--- passport-main-spring-boot/src/main/java/com/sparrow/passport/controller/SpringUserLoginController.java
+++ passport-main-spring-boot/src/main/java/com/sparrow/passport/controller/SpringUserLoginController.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
