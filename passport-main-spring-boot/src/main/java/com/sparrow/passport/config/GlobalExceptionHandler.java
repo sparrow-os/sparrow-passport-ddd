86f6a048bbd5cf514bd16aaa128e98eb24aad7dd
harry <zh_harry@163.com>
2022-10-17 20:13:07 +0800

    modify login register module

--- passport-main-spring-boot/src/main/java/com/sparrow/passport/config/GlobalExceptionHandler.java
+++ passport-main-spring-boot/src/main/java/com/sparrow/passport/config/GlobalExceptionHandler.java
@@ -1,21 +1,16 @@
 package com.sparrow.passport.config;
 
-import com.sparrow.cache.exception.CacheNotFoundException;
 import com.sparrow.protocol.BusinessException;
 import com.sparrow.protocol.Result;
 import com.sparrow.protocol.constant.Constant;
 import com.sparrow.servlet.ServletContainer;
-import java.util.Map;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
