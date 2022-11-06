901653a789df99600405b2f19c54e7e1b29bf784
harry <zh_harry@163.com>
2022-11-06 23:32:15 +0800

    init user dao remove protocol

--- passport-dao-api/src/main/java/com/sparrow/passport/dao/UserDAO.java
+++ passport-dao-api/src/main/java/com/sparrow/passport/dao/UserDAO.java
@@ -3,8 +3,6 @@ package com.sparrow.passport.dao;
 import com.sparrow.passport.po.SecurityPrincipal;
 import com.sparrow.protocol.dao.DaoSupport;
 import com.sparrow.passport.po.User;
-import com.sparrow.passport.protocol.query.UserQueryDTO;
-import java.util.List;
 
 public interface UserDAO extends DaoSupport<User, Long> {
     void save(SecurityPrincipal securityPrincipal);
