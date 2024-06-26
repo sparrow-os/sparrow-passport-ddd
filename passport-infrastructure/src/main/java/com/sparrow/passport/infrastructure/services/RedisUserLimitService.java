c0eaa95ef99f08229b19ed0f03b1821b547478e8
zh_harry <zh_harry@163.com>
2024-06-26 22:25:00 +0800

    modify login

--- passport-infrastructure/src/main/java/com/sparrow/passport/infrastructure/services/RedisUserLimitService.java
+++ passport-infrastructure/src/main/java/com/sparrow/passport/infrastructure/services/RedisUserLimitService.java
@@ -6,15 +6,6 @@ import javax.inject.Named;
 
 @Named("userLimitService")
 public class RedisUserLimitService implements UserLimitService {
-    @Override
-    public boolean canDigThread(String userId, Long threadId) throws BusinessException {
-        return true;
-    }
-
-    @Override
-    public boolean canFindPassword(Long userId) throws BusinessException {
-        return true;
-    }
 
     @Override
     public boolean canLogin(Long userId) throws BusinessException {
@@ -23,21 +14,6 @@ public class RedisUserLimitService implements UserLimitService {
 
     @Override
     public boolean canRegister(String ip) throws BusinessException {
-        return true;
-    }
-
-    @Override
-    public boolean canPublish(Long userId) throws BusinessException {
-        return true;
-    }
-
-    @Override
-    public boolean canLikeThread(Long userId, Long threadId) {
-        return true;
-    }
-
-    @Override
-    public boolean canAddCentByLogin(Long userId) {
         return true;
     }
 }
