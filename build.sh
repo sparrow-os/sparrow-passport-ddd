993dcedb721e02be1313916f171bf816cdf25e72
张立志 <zhanglizhi@tedu.cn>
2025-03-21 08:30:42 +0800

    12. 头象上传保留最新头象 19. 用户密码的格式问题 23. 分配默认头象 26. 浏览器拿到焦点后自动重连

---
+++ build.sh
@@ -0,0 +1,26 @@
+cd passport-protocol
+mvn clean install -DskipTests
+cd ../passport-api
+mvn clean install -DskipTests
+cd ../passport-dao-api
+mvn clean install -DskipTests
+cd ../passport-domain
+mvn clean install -DskipTests
+cd ../passport-authenticator
+mvn clean install -DskipTests
+cd ../passport-infrastracture
+mvn clean install -DskipTests
+cd ../passport-sparrow-dao
+mvn clean install -DskipTests
+cd ../passport-adapter
+mvn clean install -DskipTests
+cd ../passport-application
+mvn clean install -DskipTests
+cd ../passport-mybatis
+mvn clean install -DskipTests
+cd ../password-mybatis-plus
+mvn clean install -DskipTests
+cd ../passport-main-webapp
+mvn clean install -DskipTests
+cd ../passport-spring-boot
+mvn clean install -DskipTests
