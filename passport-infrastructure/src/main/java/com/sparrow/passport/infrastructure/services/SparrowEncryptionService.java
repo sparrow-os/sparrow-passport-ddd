521c0e569e2bf7eb912aff200343022b6979e4ed
张立志 <zhanglizhi@tedu.cn>
2025-05-26 21:35:12 +0800

    modify user table name

--- passport-infrastructure/src/main/java/com/sparrow/passport/infrastructure/services/SparrowEncryptionService.java
+++ passport-infrastructure/src/main/java/com/sparrow/passport/infrastructure/services/SparrowEncryptionService.java
@@ -1,36 +1,38 @@
 package com.sparrow.passport.infrastructure.services;
 
-import com.sparrow.cryptogram.Base64;
 import com.sparrow.cryptogram.MessageSignature;
 import com.sparrow.cryptogram.ThreeDES;
 import com.sparrow.passport.domain.service.EncryptionService;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+
 import javax.inject.Named;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Named("encryptionService")
 public class SparrowEncryptionService implements EncryptionService {
-    @Override public String encryptPassword(String password) {
+    @Override
+    public String encryptPassword(String password) {
         return MessageSignature.getInstance().md5(password);
     }
 
-    @Override public String base64Encode(String originCode) {
-        return Base64.encodeBytes(originCode.getBytes(StandardCharsets.US_ASCII));
+    @Override
+    public String base64Encode(String originCode) {
+        return Base64.getEncoder().encodeToString(originCode.getBytes(StandardCharsets.US_ASCII));
     }
 
-    @Override public String base64Decode(String base64) {
-        try {
-            return new String(Base64.decode(base64), StandardCharsets.US_ASCII);
-        } catch (IOException e) {
-            return null;
-        }
+    @Override
+    public String base64Decode(String base64) {
+        return new String(Base64.getDecoder().decode(base64), StandardCharsets.US_ASCII);
+
     }
 
-    @Override public String generateToken(String originCode, String originPassword) {
+    @Override
+    public String generateToken(String originCode, String originPassword) {
         return ThreeDES.getInstance().encrypt(originPassword, originCode);
     }
 
-    @Override public String decryptToken(String token, String originPassword) {
+    @Override
+    public String decryptToken(String token, String originPassword) {
         return ThreeDES.getInstance().decrypt(originPassword, token);
     }
 }
