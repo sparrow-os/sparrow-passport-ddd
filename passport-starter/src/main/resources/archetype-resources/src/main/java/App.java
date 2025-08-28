58033925298b439bc2c5c0a72b9dce263fe7d4dc
张立志 <zhanglizhi@tedu.cn>
2025-08-28 08:01:54 +0800

    modify user table name

---
+++ passport-starter/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.sparrowzoo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
