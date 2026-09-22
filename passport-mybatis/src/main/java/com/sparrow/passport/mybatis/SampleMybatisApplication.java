f76782a4dbcf0f8c83eba2ddd2cff30cb9f5e003
张立志 <zhanglizhi@tedu.cn>
2026-09-22 22:32:13 +0800

    修复 Maven 构建的 Checkstyle 校验和重复依赖
    - 补齐 89 个 Java/Properties 文件的 Apache 许可头
    - 清理未使用的 import，修正赋值空格和缩进
    - 将 MyBatis 示例控制台输出改为 SLF4J 日志
    - 移除重复的 passport-dao-api 依赖
    
    验证：mvn clean install -Dmaven.test.skip，14 个模块全部通过；
    Checkstyle 零违规，git diff --check 通过。

--- passport-mybatis/src/main/java/com/sparrow/passport/mybatis/SampleMybatisApplication.java
+++ passport-mybatis/src/main/java/com/sparrow/passport/mybatis/SampleMybatisApplication.java
@@ -1,7 +1,26 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package com.sparrow.passport.mybatis;
 
 import com.sparrow.passport.dao.UserDAO;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,18 +29,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(basePackages = {"com.sparrow.passport.dao"})
 public class SampleMybatisApplication implements CommandLineRunner {
 
-  private final UserDAO userDao;
+    private static final Logger log = LoggerFactory.getLogger(SampleMybatisApplication.class);
 
-  public SampleMybatisApplication(UserDAO userDao) {
-    this.userDao = userDao;
-  }
+    private final UserDAO userDao;
 
-  public static void main(String[] args) {
-    SpringApplication.run(SampleMybatisApplication.class, args);
-  }
+    public SampleMybatisApplication(UserDAO userDao) {
+        this.userDao = userDao;
+    }
 
-  @Override
-  public void run(String... args) throws Exception {
-    System.out.println(this.userDao.getEntity(32L));
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SampleMybatisApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("{}", this.userDao.getEntity(32L));
+    }
 }
